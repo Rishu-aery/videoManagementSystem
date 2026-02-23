21b829d1b65d06ceb421f56dcaa952e84a81146d
Rishabh-aery <rishab.arrey@quodd.com>
2026-02-24 00:31:48 +0530

    created user and video model

---
+++ src/config/server-config.js
@@ -0,0 +1,7 @@
+export const PORT = process.env.PORT || 8000;
+export const MONGODB_URI = process.env.MONGODB_URI;
+export const CORS_ORIGIN = process.env.CORS_ORIGIN || '*';
+export const ACCESS_TOKEN_SECRETE = process.env.ACCESS_TOKEN_SECRETE;
+export const ACCESS_TOKEN_EXPIRY = process.env.ACCESS_TOKEN_EXPIRY || '1d';
+export const REFRESH_TOKEN_SECRETE = process.env.REFRESH_TOKEN_SECRETE;
+export const REFRESH_TOKEN_EXPIRY = process.env.REFRESH_TOKEN_EXPIRY || '10d';
